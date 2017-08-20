c60dd2acae084a4dc5cfb6a824ccd7947fa416f3
Manabu Nakazawa <mshibanami+git@gmail.com>
2017-08-20 15:51:31 +0900

    language

--- lib/language.rb
+++ lib/language.rb
@@ -1,12 +1,12 @@
 class Language
     CODE_TO_ORIGINAL_NAME_MAP ||= {
-        en: 'English',
-        ja: '日本語'
+        'en' => 'English',
+        'ja' => '日本語'
     }.freeze
 
     CODE_TO_REGION_MAP ||= {
-        en: 'US',
-        ja: 'JP'
+        'en' => 'US',
+        'ja' => 'JP'
     }.freeze
 
     attr_reader :code
