1df797b285b5ac7bfbabd9ab7fea80c5dcc05fd5
Manabu Nakazawa <mshibanami+git@gmail.com>
2017-08-20 14:54:16 +0900

    language, keywords

--- lib/item_rule.rb
+++ lib/item_rule.rb
@@ -1,13 +1,13 @@
-class RuleParser
-    # Path for output/
-    attr_reader :output_path
-
+class ItemRule
     # Path of the layout file for specified item
     attr_reader :layout_path
 
+    # Path for output/
+    attr_reader :output_path
+
     # Language code
     # nil if the specified item is localized
-    attr_reader :language_code
+    attr_reader :language
 
     def initialize(item, config)
         langs = config[:supported_languages].join('|')
@@ -21,18 +21,26 @@ class RuleParser
         when %r{^/(?<id>.*)/(?<lang>#{langs})\..*(#{doc_extensions})$}
             # Full localized pages like the homepage or the blog's top page
             @layout_path = default_layout
-            @lang = $~[:lang]
             @output_path = "/#{$~[:lang]}/#{$~[:id]}.html"
+            @language = Language.new($~[:lang])
         when %r{^/(?<dir>.*)/(?<lang>#{langs})/(?<name>.*)\.(#{doc_extensions})}
             # Non localized pages like blog's articles
             @layout_path = default_layout
             id = $~[:dir] + '/' + $~[:name]
-            @lang = $~[:lang]
             @output_path = "/#{$~[:lang]}/#{id}/index.html"
+            @language = Language.new($~[:lang])
         when /.*(scss|sass)/
             @output_path = "/assets/css/compiled/#{File.basename(item.identifier.to_s, '.scss')}.css"
         else
             @output_path = item.identifier.to_s
         end
+    end
+
+    def language_code
+        language.code
+    end
+
+    def region_code
+        language.region_code
     end
 end
