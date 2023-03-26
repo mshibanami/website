ccb610144d31609fb5c2ee55739e5786f780d89d
Manabu Nakazawa <mshibanami+git@gmail.com>
2023-03-26 22:41:45 +1000

    Add redirect rule to the new support website

--- next.config.js
+++ next.config.js
@@ -1,2 +1,11 @@
 module.exports = {
+    async redirects() {
+        return [
+            {
+                source: '/projects/redirect-web-for-safari',
+                destination: 'https://mshibanami.github.io/redirect-web',
+                permanent: true,
+            },
+        ]
+    },
 };
